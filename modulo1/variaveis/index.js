931df837e45976dfba314dfc9eb58591abc8d449
AlessandroSilvino <102526803+AlessandroSilvino@users.noreply.github.com>
2022-04-12 19:46:29 -0300

    Update index.js

--- modulo1/variaveis/index.js
+++ modulo1/variaveis/index.js
@@ -18,60 +18,60 @@ variavel "t" se torna "honorarioDiario"
 // EXERCÍCIOS DE ESCRITA DE CÓDIGO 
 
 // EXERCÍCIO 1
-//  let nome; 
-//  let idade; 
+  let nome; 
+  let idade; 
 
-//  console.log (typeof nome);
-//  console.log (typeof idade);
+  console.log (typeof nome);
+  console.log (typeof idade);
 
 /*ambas as variaveis foram indicadas como "undefined" pelo fato de não possuirem valores atribuidos
  a elas */
 
-// nome = prompt ("Qual o seu nome?");
-//  idade = prompt("Qual a sua idade?");
+ nome = prompt ("Qual o seu nome?");
+ idade = prompt("Qual a sua idade?");
 
-//  console.log (typeof nome);
-//  console.log (typeof idade);
+  console.log (typeof nome);
+  console.log (typeof idade);
 
-//  //o programa apresentou ambas as variáveis como sendo do tipo "string", pois esse é o formato armazenado
-//  pelo comando prompt
+/*o programa apresentou ambas as variáveis como sendo do tipo "string", pois esse é o formato armazenado
+pelo comando "prompt"*/
 
-//  console.log ("Olá",nome,",","você tem", idade, "anos");
+console.log ("Olá",nome,",","você tem", idade, "anos");
 
 
 // EXERCÍCIO 2
 
-// let pergunta1 = "Você está sentado agora?";
-// let pergunta2 = "Você está usando camisa preta?";
-// let pergunta3 = "Você tomou café hoje?";
+ let pergunta1 = "Você está sentado agora?";
+ let pergunta2 = "Você está usando camisa preta?";
+ let pergunta3 = "Você tomou café hoje?";
 
-// let resp1 = prompt (pergunta1);
-// let resp2 = prompt (pergunta2);
-// let resp3 = prompt (pergunta3);
+ let resp1 = prompt (pergunta1);
+ let resp2 = prompt (pergunta2);
+ let resp3 = prompt (pergunta3);
 
-// console.log (pergunta1, resp1);
-// console.log (pergunta2, resp2);
-// console.log (pergunta3, resp3);
+ console.log (pergunta1, resp1);
+ console.log (pergunta2, resp2);
+ console.log (pergunta3, resp3);
 
 // EXERCÍCIO 3
 
-// let a = 10
-// let b = 25
+ let a = 10
+ let b = 25
 
-// // Aqui faremos uma lógica para trocar os valores
-// let c = a
-// a = b
-// b = c
+// Aqui faremos uma lógica para trocar os valores
+ let c = a
+ a = b
+ b = c
 
-// // Depois de trocados, teremos o seguinte resultado:
-// console.log("O novo valor de a é", a) // O novo valor de a é 25
-// console.log("O novo valor de b é", b) // O novo valor de b é 10
+// Depois de trocados, teremos o seguinte resultado:
+ console.log("O novo valor de a é", a) // O novo valor de a é 25
+ console.log("O novo valor de b é", b) // O novo valor de b é 10
 
 // DESAFIO
 
-// let numero1 = Number (prompt ("Digite o primeiro número: "))
-// let numero2 = Number (prompt ("Digite o segundo número: "))
+ let numero1 = Number (prompt ("Digite o primeiro número: "))
+ let numero2 = Number (prompt ("Digite o segundo número: "))
 
 
-// console.log (numero1 + numero2)
-// console.log (numero2 * numero2)
+ console.log (numero1 + numero2)
+ console.log (numero2 * numero2)
