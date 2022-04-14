fd3d98c848a6e9e323c5749516fbb4ce420d213a
Alessandro <eng.alessandrosilvino@gmail.com>
2022-04-14 19:21:48 -0300

    Exercícios de operadores

---
+++ modulo1/operadores/index.js
@@ -0,0 +1,60 @@
+// OPERADORES--------------------------------------------------------------------------------------------------
+
+//Exercícios de interpretação de código------------------------------------------------------------------------
+
+// 1. 
+// a. false
+// b. false
+// c. true
+// d. boolean
+
+// 2. 
+// o comando "prompt" armazena informações em formato de String, o que impede que a operação matemática de soma 
+// seja realizada da forma correta (somando dois NÚMEROS). 
+
+// 3. 
+// É necessário que o colega converta a informação armazenada pelo prompt (string) em um número,
+//utilizando o comando "Number(prompt...)"
+
+
+//Exercícios de escrita de código------------------------------------------------------------------------------
+// 1ª questão--------------------------------------------------------------------------------------------------
+// let idade = Number(prompt("Qual a sua idade?"))
+// let idadeMelhorAmig = Number(prompt("Qual a idade do seu melhor amigo(a)?"))
+
+// const comparacao = idade > idadeMelhorAmig
+// let diferenca = (idade - idadeMelhorAmig)
+
+// console.log("Sua idade é maior do que a do seu melhor amigo?", comparacao)
+// console.log("A diferença de suas idades é:", diferenca)
+
+// 2ª questão-------------------------------------------------------------------------------------------------- 
+// let numeroPar = prompt("Insira um número par: ")
+
+// console.log (numeroPar%2)
+
+//o resto de qualquer número PAR divido por 2 será sempre 0.
+//se inserirmos qualquer número ÍMPAR, o resto será sempre 1.
+
+// 3ª questão--------------------------------------------------------------------------------------------------
+// let idade = Number(prompt ("Qual a sua idade?"))
+
+// console.log (idade*12)
+// console.log (idade*365)
+// console.log (idade*365*24)
+
+// 4ª questão--------------------------------------------------------------------------------------------------
+// let numero1 = Number (prompt ("Digite o primeiro número: "))
+// let numero2 = Number(prompt ("Digite o segundo número: "))
+
+// const maior = numero1 > numero2
+// const igual = numero1 === numero2
+// const divisivel1 = numero1%numero2 === 0
+// const divisivel2 = numero2%numero1 === 0
+
+// console.log (maior)
+// console.log (igual)
+// console.log (divisivel1)
+// console.log (divisivel2)
+
+
